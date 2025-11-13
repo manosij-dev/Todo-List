5f55a0e2429da1f81ec3816fbda5a0e4e484c58b
Manosij <manosijchowdhury@gmail.com>
2025-11-13 21:54:41 +0530

    Updated Styling

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -12,8 +12,8 @@ const TodoForm = ({addTodo}) => {
   }
   return (
     <div className='mt-10 text-center'>
-     <input className='border-solid h-10 w-100 bg-gray-300 p-5 rounded outline-none' type="text" value={value} placeholder="Enter a task" onChange={(e)=>setValue(e.target.value)} />
-     <button className='ml-3 bg-green-400 rounded p-2 scale-95' onClick={createTodo}>Add Todo</button>
+     <input className='border-solid h-10 w-100 border border-black p-5 rounded outline-none' type="text" value={value} placeholder="Enter a task" onChange={(e)=>setValue(e.target.value)} />
+     <button className='ml-3 bg-green-400 rounded p-2 active:scale-95 text-white' onClick={createTodo}>Add Todo</button>
     </div>
   )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,14 @@ const TodoList = ({todos, delToDo}) => {
   return (
     <div className="mt-10 flex flex-col items-center w-full">
         {todos.map((item)=>(
-          <div className="flex justify-between items-center w-1/3 bg-white shadow-md border border-gray-200 rounded-xl px-5 py-3 mt-3 transition hover:shadow-lg" key={item.id}>
+          <div className="flex justify-between items-center w-1/3 bg-white shadow-md border border-gray-200 rounded-xl px-5 py-3 mt-3 " key={item.id}>
             <span className="text-xl font-medium text-gray-800">
               {item.text}
             </span>
-            <button className="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded-lg transition" onClick={()=>delToDo(item.id)}>Delete</button>
+            <div>
+              <button className="bg-red-500 scale-95 text-white font-semibold px-4 py-2 rounded-lg" onClick={()=>delToDo(item.id)}>Delete</button>
+              <button className="bg-orange-500 scale-95 text-white font-semibold px-4 py-2 rounded-lg">Edit</button>
+            </div>
           </div>
         ))}
     </div>
